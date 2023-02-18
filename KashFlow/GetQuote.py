632fe44a6a53eb642e33dae66fb6f6bacfd6c417
lukemodena <97751351+lukemodena@users.noreply.github.com>
2023-02-18 18:41:55 +0000

    Create GetQuote.py

---
+++ KashFlow/GetQuote.py
@@ -0,0 +1,11 @@
+my_headers = {
+    'Accept' : 'application/json',
+    'Content-Type' : 'application/json',
+    'Authorization' : input_data['token'],
+    'Connection': 'keep-alive',
+    'Origin' : 'chrome-extension: //rest-console-id',
+    'User-Agent' : 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.22 (KHTML, like Gecko) Chrome/25.0.1364.152 Safari/537.22'
+}
+response = requests.get(f'https://api.kashflow.com/v2/quotes/{input_data['QuoteID']}', headers=my_headers)
+
+output = response.json()
