43d17b6276606691bc0098464cd0a82162dad8eb
lukemodena <97751351+lukemodena@users.noreply.github.com>
2023-02-18 20:48:33 +0000

    Create DeleteQuote.py

---
+++ KashFlow/Quotes/DELETE/DeleteQuote.py
@@ -0,0 +1,14 @@
+my_headers = {
+    'Accept' : 'application/json',
+    'Content-Type' : 'application/json',
+    'Authorization' : input_data['token'],
+    'Connection': 'keep-alive',
+    'Origin' : 'chrome-extension: //rest-console-id',
+    'User-Agent' : 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.22 (KHTML, like Gecko) Chrome/25.0.1364.152 Safari/537.22'
+}
+response = requests.delete(f'https://api.kashflow.com/v2/quotes/{input_data['QuoteID']}', headers=my_headers)
+
+output = [{'result': 'done', 'response': response}]
+
+return output
+
