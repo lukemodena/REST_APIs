536eb71665f0c4823eeb98f370b6c1bbd2bca7c2
lukemodena <97751351+lukemodena@users.noreply.github.com>
2023-02-18 18:26:18 +0000

    Create GETLatestQuote.py

---
+++ KashFlow/GETLatestQuote.py
@@ -0,0 +1,11 @@
+my_headers = {
+    'Accept' : 'application/json',
+    'Content-Type' : 'application/json',
+    'Authorization' : input_data['token'],
+    'Connection': 'keep-alive',
+    'Origin' : 'chrome-extension: //rest-console-id',
+    'User-Agent' : 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.22 (KHTML, like Gecko) Chrome/25.0.1364.152 Safari/537.22'
+}
+response = requests.get('https://api.kashflow.com/v2/quotes?page=1&perpage=1&sortby=Number&order=Desc', headers=my_headers)
+
+output = response.json()
